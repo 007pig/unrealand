c436e6a8eefffb8a31137b7571d657840ac1d978
Xiaoyu Huang <xiaoyu.huang@btcchina.com>
2014-07-31 23:55:37 +0800

    bitmessage api is working

--- server/sockets/test.js
+++ server/sockets/test.js
@@ -1,6 +1,10 @@
+var bitmessage = require('bitmessage-node')('localhost', 8442, 'user', 'password');
+
 module.exports = function(io) {
 	io.on('connection', function (socket) {
-		socket.emit('news', { hello: 'world' });
+		bitmessage.addresses.list(function(value) {
+			socket.emit('news', value);
+		});
 		socket.on('my other event', function (data) {
 			console.log(data);
 		});
